6f30fdb128769e0f92a00d34d369308a21e0b6ec
ezzeldine22 <ezzeldinereda@gmail.com>
2025-10-04 19:24:44 +0300

    mergepls

--- Backend/E-Commerce/obj/Debug/net8.0/E-Commerce.AssemblyInfo.cs
+++ Backend/E-Commerce/obj/Debug/net8.0/E-Commerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+915992588f6f2df1274a35d435db67f9995afa00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deb3fc9fb49bfbb374d2992fd8ce44db15eccec4")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
